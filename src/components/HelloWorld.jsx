17f7a779e051a18a03990e74624ba04be5c508f4
addleonel <cambroni3633@gmail.com>
2021-04-27 18:52:44 -0500

    beginning

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const HelloWorld = () =>{
+    return (
+        <div>
+        <h1>Hello World!</h1>
+        <p>my Parraph</p>
+        </div>
+    );
+};
+
+export default HelloWorld;
