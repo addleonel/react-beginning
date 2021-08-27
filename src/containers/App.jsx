9c2090a19a17d075c35144ab105438ca1bf4eb89
addleonel <cambroni3633@gmail.com>
2021-08-27 10:39:03 -0500

    custom hooks

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,23 +8,21 @@ import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 
+// hooks
+import useInitialState from '../hooks/useInitialState';
+
 // styles
 import '../assets/styles/App.scss';
 
+const API = 'http://localhost:3000/initialState/';
+
 const App = () => {
-    const [videos, setVideos ] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost:3000/initialState/')
-            .then(response => response.json())
-            .then(data => setVideos(data));
-    },[]);
-
+    const initialState = useInitialState(API);
     return (
         <div className="App">
             <Header/>
             <Search/>
-            {videos.mylist?.length > 0 &&
+            {initialState.mylist?.length > 0 &&
             <Categories title="Mi Lista">
                 <Carousel>
                     <CarouselItem/>
@@ -33,14 +31,14 @@ const App = () => {
             }
             <Categories title="Tendencias">
                 <Carousel>
-                    { videos.trends?.map(item => 
+                    { initialState.trends?.map(item => 
                     <CarouselItem key={item.id} {...item}/>
                     )}
                 </Carousel>
             </Categories>
             <Categories title="Originales">
                 <Carousel>
-                    {videos.originals?.map(item =>    
+                    {initialState.originals?.map(item =>    
                     <CarouselItem key={item.id} {...item}/>
                     )}
                 </Carousel>
